052e7af60cd6ef642eb83bfa27d32c1d0368cf0a
hamza5645 <97033430+hamza5645@users.noreply.github.com>
2023-01-21 21:47:47 +0200

    created a landing page for provin

--- Provin/ContentView.swift
+++ Provin/ContentView.swift
@@ -10,12 +10,62 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 164, height: 34)
+                    .padding(15)
+                
+                Spacer()
+                
+                Image("menu")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .padding(15)
+            }
+            
+            VStack {
+                Text("Hire The World's Top Calibers")
+                    .font(.system(size: 47))
+                    .fontWeight(.semibold)
+                
+                Image("image")
+                    .resizable()
+                    .frame(width: 500, height: 400)
+                Spacer()
+                
+                HStack {
+                    Button {
+                        //code
+                    }label: {
+                        Rectangle()
+                            .fill(.black)
+                            .frame(width: 150, height: 70)
+                            .overlay(
+                                Text("Hire a Caliber")
+                                    .foregroundColor(.white)
+                            )
+                    }
+                    .padding()
+                    
+                    
+                    Button {
+                        //code
+                    }label: {
+                        Rectangle()
+                            .fill(.white)
+                            .border(Color.black, width: 2)
+                            .frame(width: 150, height: 70)
+                            .overlay(
+                                Text("Join as a Caliber")
+                                    .foregroundColor(.black)
+                            )
+                    }
+                    .padding()
+
+                }
+            }
         }
-        .padding()
     }
 }
 
