bb165bb8e09156fcd30ac5dee4db6f3e3ba5f818
hamza5645 <97033430+hamza5645@users.noreply.github.com>
2023-01-21 21:01:10 +0200

    Initial Commit

---
+++ Provin/ProvinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProvinApp.swift
+//  Provin
+//
+//  Created by Hamza Osama on 21/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ProvinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
